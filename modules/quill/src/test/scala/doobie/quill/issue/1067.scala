dca730c35ccc38ab3c7abe876f20731fb472c22c
Jakub Kozłowski <kubukoz@gmail.com>
2021-11-04 19:34:18 +0100

    Initial code dump

---
+++ modules/quill/src/test/scala/doobie/quill/issue/1067.scala
@@ -0,0 +1,32 @@
+package doobie.quill
+package issue
+
+import cats.effect._
+import doobie._
+import doobie.implicits._
+import io.getquill._
+
+// https://github.com/tpolecat/doobie/issues/1067
+class `1067` extends munit.FunSuite {
+
+  import cats.effect.unsafe.implicits.global
+
+  lazy val xa = Transactor.fromDriverManager[IO](
+    "org.postgresql.Driver",
+    "jdbc:postgresql:world",
+    "postgres", ""
+  )
+
+  val dc = new DoobieContext.Postgres(Literal)
+  import dc._
+
+  case class Country(name: String, indepYear: Option[Short])
+
+  test("Issue1067 - correctly select many countries, with a null in last position") {
+    val stmt     = quote { query[Country] }
+    val actual   = run(stmt).transact(xa).unsafeRunSync()
+    assertEquals(actual.count(_.indepYear.isDefined), 192)
+    assertEquals(actual.count(_.indepYear.isEmpty), 47)
+  }
+
+}
